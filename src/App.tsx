228e74a62322fabf2269e82f77901e4336c3d4c4
Sindoval Alves <sindoval321@gmail.com>
2024-05-20 19:36:57 -0300

    Requisito 1 concluído

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Title>Gerenciador de senhas</Title>
     </div>
   );
 }
 
 export default App;
-// Iniciando o projeto
